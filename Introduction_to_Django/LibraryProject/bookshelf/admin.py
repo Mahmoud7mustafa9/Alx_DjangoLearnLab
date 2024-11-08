e14c93a7993936ee339a3ad8ff994fde8b709ccf
mahmoud moustafa <7odamostafa97@gmail.com>
2024-11-08 16:01:26 +0200

    ef

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+admin.site.register(Book)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+class Book(admin.ModelAdmin):
+    search_field=["author"]
+    list_filter=["title","publication_year"]
