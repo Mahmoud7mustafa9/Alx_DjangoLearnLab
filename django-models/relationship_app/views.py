91f4797f73931f59f0649700b209d2082c004a4c
mahmoud moustafa <7odamostafa97@gmail.com>
2024-11-10 16:51:54 +0200

    ftrbf

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 # Create your views here.
 from .models import Book
 from .models import Library
-from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 
 # Function-based View to list all books
@@ -13,18 +12,10 @@ def list_books (request):
     return render(request, "relationship_app/list_books.html", {'books': books})
 
 class LibraryDetailView(DetailView):
-    model = Book
+    model = Library
     template_name = "relationship_app/library_detail.html"
-    context_object_name = 'books'
-
-    def get_queryset(self):
-        # Get the library by the ID passed in the URL and then return books for that library
-        library_id = self.kwargs['library_id']
-        return Book.objects.filter(library__id=library_id)
-
+    context_object_name = 'library'
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        library_id = self.kwargs['library_id']
-        library = Library.objects.get(id=library_id)
-        context['library'] = library
+        context['book'] = self.object.books.all()
         return context
