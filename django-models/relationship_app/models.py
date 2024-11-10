27705918616deadf9c20a872894a6337341b806e
mahmoud moustafa <7odamostafa97@gmail.com>
2024-11-10 16:08:08 +0200

    fv

---
+++ django-models/relationship_app/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.model):
+    name=models.CharField(max_length=200)
+
+
+class Book(models.model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete= models.CASCADE )
+
+
+class Library(models.model):
+    name=models.CharField(max_length=100)
+    books=models.ManyToManyField(Book,related_name= name)
+
+class Librarian(models.Model):
+    name=models.CharField(max_length=100)
+    library = models.OneToOneField(Library)
+# Author Model:
+# name: CharField.
+# Book Model:,
+# title: CharField.
+# author: ForeignKey to Author.
+
+# Library Model:
+# name: CharField.
+# books: ManyToManyField to Book.
+
+# Librarian Model:
+# name: CharField.
+# library: OneToOneField to Library.
+
+# Apply Database Migrations:
+
+# Run migrations to create your model tables: python manage.py makemigrations relationship_app followed by python manage.py migrate.
+# Implement Sample Queries:
+
+# Prepare a Python script query_samples.py in the relationship_app directory. This script should contain the query for each of the following of relationship:
+# Query all books by a specific author.
+# List all books in a library.
+# Retrieve the librarian for a library.
