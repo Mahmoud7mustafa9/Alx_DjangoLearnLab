36afd7cc4d198ee6401d7b333d13ed5119d6a656
mahmoud moustafa <7odamostafa97@gmail.com>
2024-11-11 16:51:32 +0200

    wef

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import list_books
 from .views import LibraryDetailView
 from . import views
-from .views import Register
+from .views import register
 from django .contrib.auth.views import LoginView
 from django .contrib.auth.views import LogoutView
 
@@ -12,7 +12,7 @@ urlpatterns = [
 
     # URL pattern for the class-based view to display books in a specific library
     path('library/', LibraryDetailView.as_view(), name='library_detail'),
-    path('register/', views.Register.as_view(),name="register"),
+    path('register/', views.register.as_view(),name="register"),
     path('login/',LoginView.as_view(template_name='relationship_app/login.html'),name='login'),
     path('logout/',LogoutView.as_view(template_name='relationship_app/logout.html'),name='logout'),
     path('admin/',views.admin_view,name='admin_view/'),
