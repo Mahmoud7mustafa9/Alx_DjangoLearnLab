6a2dbb11b2dbf774238613b73550c1ede808758c
mahmoud moustafa <7odamostafa97@gmail.com>
2024-11-12 15:49:10 +0200

    sdw

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -32,7 +32,7 @@ class Librarian(models.Model):
         return self.name 
 
 class UserProfile(models.Model):
-    role=models.CharField(max_length=100)
+    role=models.CharField(max_length=100, choices=[('LIbrarian','Librarian'),('Admin','Admin'),('Member','Member')])
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     def create_user_profile(sender, instance,created, **kwargs):
         if created:
