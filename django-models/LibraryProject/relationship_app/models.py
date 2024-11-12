27ac4a1a502e3eaea4067d4d502b081f8a5b7c11
mahmoud moustafa <7odamostafa97@gmail.com>
2024-11-12 15:50:21 +0200

    dfv

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -13,8 +13,8 @@ class Book(models.Model):
     author = models.ForeignKey(Author, on_delete= models.CASCADE )
     def __str__(self):
         return self.title
-    class meta:
-        premissions =[
+    class Meta:
+        permissions =[
             ('can_change_book','can change book'),
             ('can_add_book','can add book'),
             ('can_delete_book','can delete book'),
