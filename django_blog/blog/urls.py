91997bdd757cbea602b36325751af38a2dd034f7
mahmoud moustafa <7odamostafa97@gmail.com>
2024-12-06 23:47:30 +0200

    sde

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -33,8 +33,8 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post-list'),
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
-    path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+    path("post/<int:pk>/", views.PostDetailView.as_view(), name='post-detail'),
+    path("post/new/", views.PostCreateView.as_view(), name='post-create'),
+    path("post/<int:pk>/edit/", views.PostUpdateView.as_view(), name='post-update'),
+    path("post/<int:pk>/delete/", views.PostDeleteView.as_view(), name='post-delete'),
 ]
