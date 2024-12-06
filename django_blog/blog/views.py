658230399102de09255b8f565eda978e1b214c71
mahmoud moustafa <7odamostafa97@gmail.com>
2024-12-06 23:21:14 +0200

    fdeef

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')  # Redirect to profile page
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        user = request.user
+        user.email = request.POST.get('email')
+        user.save()
+    return render(request, 'profile.html', {'user': request.user})
