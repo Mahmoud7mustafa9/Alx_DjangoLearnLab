b7c9be18af8edc6546fa289ba0a9ff129dd757e2
mahmoud moustafa <7odamostafa97@gmail.com>
2024-12-06 21:53:58 +0200

    x d

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api.urls import urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+["api.urls"]
