c64e7ae8a511ebb794095d192b59241eee21ab4a
mahmoud moustafa <7odamostafa97@gmail.com>
2024-12-06 23:56:14 +0200

    rf

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -49,7 +49,8 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post-list'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
-    path('post/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
+    path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
     path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
 ]
+["post/<int:pk>/comments/new/"]
