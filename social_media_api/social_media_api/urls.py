0a2cadf1ce0062a019b366f813b935c6d1d06aa8
mahmoud moustafa <7odamostafa97@gmail.com>
2024-12-15 17:12:02 +0200

    df

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('posts/', include('posts.urls')),
+    path('api/', include('posts.urls')),
 ]
 
 from rest_framework import permissions
