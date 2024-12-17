33d7632b33a7a9671e18534284f12ae525629aa0
mahmoud moustafa <7odamostafa97@gmail.com>
2024-12-17 16:25:54 +0200

    d

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -164,3 +164,5 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_STORAGE_BUCKET_NAME = 'your-bucket-name'
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+
+"PORT"
